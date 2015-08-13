eab01c2f99ff6ba4ba60fc8af55ca463db61e7f5
Peet <peter.goddard@contactpartners.com>
2015-08-13 12:23:23 +0100

    v1

---
+++ index.js
@@ -0,0 +1,13 @@
+module.exports = {
+  parse: function(querystring) {
+    if (/^[?#]/.test(querystring)) {
+      querystring = querystring.substring(1);
+    }
+
+    return querystring.split('&').reduce(function(a, pair) {
+      var parts = pair.split('=');
+      a[parts[0]] = decodeURIComponent(parts[1]);
+      return a;
+    }, {})
+  }
+};
