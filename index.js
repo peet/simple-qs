351e21522a582c18ba73cb9c7a599cc9ab7b7926
Peet <peter.goddard@contactpartners.com>
2015-08-13 12:25:43 +0100

    add caching - v1.1

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
+var cache = {};
+
 module.exports = {
   parse: function(querystring) {
     if (/^[?#]/.test(querystring)) {
       querystring = querystring.substring(1);
     }
 
-    return querystring.split('&').reduce(function(a, pair) {
+    return cache[querystring] || (cache[querystring] = querystring.split('&').reduce(function(a, pair) {
       var parts = pair.split('=');
       a[parts[0]] = decodeURIComponent(parts[1]);
       return a;
-    }, {})
+    }, {}));
   }
 };
